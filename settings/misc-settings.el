c5658ce194047ecd3b507d03f74ebb24c33d73dd
João Pedro Foscarini <jfoscarini@gmail.com>
2015-10-27 20:09:13 -0200

    Backup and autosave files should be in .emacs.d

--- settings/misc-settings.el
+++ settings/misc-settings.el
@@ -1,2 +1,23 @@
+;; Backup and Autosave files should be in .emacs.d
+(make-directory "~/.emacs.d/autosaves/" t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+
+(make-directory "~/.emacs.d/backups/" t)
+
+(setq  backup-directory-alist '((".*" . "~/.emacs.d/backups/"))
+       version-control t
+       delete-old-versions t
+       kept-new-versions 4
+       kept-old-versions 1)
+
+;; Disable for sudo and su
+(setq backup-enable-predicate
+      (lambda (name)
+	(and (normal-backup-enable-predicate name)
+	     (not
+	      (let ((method (file-remote-p name 'method)))
+		(when (stringp method)
+		  (member method '("su" "sudo"))))))))
+
 ;; Trailing whitespaces are not acceptable
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
