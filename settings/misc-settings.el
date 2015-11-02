4889c2ea73f98a70e241a605a4b6d7f5cb9b30fa
João Pedro Foscarini <jfoscarini@gmail.com>
2015-11-02 16:07:46 -0200

    Moving multi-term config to shell-settings

--- settings/misc-settings.el
+++ settings/misc-settings.el
@@ -36,8 +36,5 @@
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
-;; Multi-term works better with ZSH
-(use-package multi-term)
-
 ;; Trailing whitespaces are not acceptable
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
--- settings/shell-settings.el
+++ settings/shell-settings.el
@@ -1,3 +1,6 @@
 ;; Use bash as the default shell program
 (when (system-is-linux)
   (setq shell-file-name "/bin/bash"))
+
+;; Multi-term works better with ZSH
+(use-package multi-term)
